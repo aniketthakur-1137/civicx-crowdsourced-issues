735f9cb3ee4e33d9ca6381ee632797108d3f58fb
aniketthakur-1137 <aniketthakur91540@gmail.com>
2026-05-15 08:58:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
